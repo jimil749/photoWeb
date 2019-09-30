6afcf8069576b37a02d5adfae97c1d76f73e3b9a
Jimil Desai <jimildesai42@gmail.com>
2019-10-01 00:02:45 +0530

    First Commit

---
+++ js/myScripts.js
@@ -0,0 +1,62 @@
+//jquery script
+
+$(document).ready(function(){
+  $('#previous').on('click', function(){
+    $('#im_' + currentImage).stop().fadeOut(1);
+    decreaseImage();
+    $('#im_'+currentImage).stop().fadeIn(1);
+  });
+  $('#next').on('click', function(){
+    $('#im_' + currentImage).stop().fadeOut(1);
+    increaseImage();
+    $('#im_' + currentImage).stop().fadeIn(1);
+  });
+
+  var currentImage = 1;
+  var totalImages = 3;
+
+  function increaseImage() {
+    ++currentImage;
+    if (currentImage > totalImages){
+      currentImage = 1;
+    }
+  }
+
+  function decreaseImage() {
+    --currentImage;
+    if (currentImage < 1){
+      currentImage = totalImages;
+    }
+  }
+});
+
+// var slideIndex = 1;
+// showSlides(slideIndex);   //js function to show slides one by one.
+//
+// function plusSlides(n){
+//   showSlides(slideIndex += n);
+// }
+//
+// function currentSlide(n){
+//   showSlides(slideIndex = n);
+// }
+//
+// function showSlides(n){
+//   var i;
+//   var slides = document.getElementByClassName("mySlide"); //this returns the collection of all "mySlide" as node object.
+//   var dots = document.getElementsByClassName("dots");//get dots
+//   if (n > slides.length){
+//     slideIndex = 1;
+//   }
+//   if (n < 1){
+//     slideIndex = slides.length;
+//   }
+//   for (i = 0; i < slides.length; i++){
+//     slides[i].style.display = "none";
+//   }
+//   for (i = 0; i < dots.length; i++){
+//     dots[i].className = dots[i].className.replace("active", "")
+//   }
+//   slides[slideIndex-1].style.display = "block";
+//   dots[slideIndex-1].className += " active";
+// }
