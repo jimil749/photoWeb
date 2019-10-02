0224cc1fec940c94e6cdb0986ecd7bfe0d16989d
Jimil Desai <jimildesai42@gmail.com>
2019-10-02 20:09:30 +0530

    Second Commit, Debugged Slideshow and Styling

--- js/myScripts.js
+++ js/myScripts.js
@@ -1,5 +1,5 @@
 //jquery script
-
+//
 $(document).ready(function(){
   $('#previous').on('click', function(){
     $('#im_' + currentImage).stop().fadeOut(1);
@@ -13,7 +13,7 @@ $(document).ready(function(){
   });
 
   var currentImage = 1;
-  var totalImages = 3;
+  var totalImages = 6;
 
   function increaseImage() {
     ++currentImage;
@@ -30,33 +30,38 @@ $(document).ready(function(){
   }
 });
 
-// var slideIndex = 1;
-// showSlides(slideIndex);   //js function to show slides one by one.
+// var slideIndex = 0;
+// showSlides();   //js function to show slides one by one.
 //
-// function plusSlides(n){
-//   showSlides(slideIndex += n);
-// }
-//
-// function currentSlide(n){
-//   showSlides(slideIndex = n);
-// }
+// // function plusSlides(n){
+// //   showSlides(slideIndex += n);
+// // }
+// //
+// // function currentSlide(n){
+// //   showSlides(slideIndex = n);
+// // }
 //
 // function showSlides(n){
 //   var i;
 //   var slides = document.getElementByClassName("mySlide"); //this returns the collection of all "mySlide" as node object.
 //   var dots = document.getElementsByClassName("dots");//get dots
-//   if (n > slides.length){
-//     slideIndex = 1;
-//   }
-//   if (n < 1){
-//     slideIndex = slides.length;
-//   }
+//   // if (n > slides.length){
+//   //   slideIndex = 1;
+//   // }
+//   // if (n < 1){
+//   //   slideIndex = slides.length;
+//   // }
 //   for (i = 0; i < slides.length; i++){
 //     slides[i].style.display = "none";
+//   }
+//   slideIndex++;
+//   if (slideIndex > slides.length){
+//     slideIndex = 1;
 //   }
 //   for (i = 0; i < dots.length; i++){
 //     dots[i].className = dots[i].className.replace("active", "")
 //   }
 //   slides[slideIndex-1].style.display = "block";
 //   dots[slideIndex-1].className += " active";
+//   setTimeout(showSlides,2000);
 // }
