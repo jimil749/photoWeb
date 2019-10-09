b6eff253ecf94c53e21574b50e6abeeb4c526ebe
Jimil Desai <jimildesai42@gmail.com>
2019-10-09 18:05:57 +0530

    New Commit

---
+++ js1/myScripts.js
@@ -0,0 +1,48 @@
+window.onscroll = function(){myFunc};
+var header = document.getElementById("mytopnav");
+var sticky = header.offsetTop;
+function myFunc(){
+  if (window.pageYoffset > sticky){
+    header.classList.add("sticky");
+  }
+  else{
+    header.classList.remove("sticky");
+  }
+}
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
