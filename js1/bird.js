1988ee35a85182e5022e19e539f190ec095053ec
Jimil Desai <jimildesai42@gmail.com>
2019-10-13 15:40:30 +0530

    New Commit

--- js1/bird.js
+++ js1/bird.js
@@ -18,3 +18,62 @@ function myFunction() {
     x.className = "topnav";
   }
 }
+
+function switchPage(){
+  var x = document.getElementById("ro");
+  var y = document.getElementById("ro1");
+//  console.log(x.style.display);
+  if (x.style.display === "none"){
+    x.style.display = "flex";
+    y.style.display = "none";
+  }
+  else{
+    x.style.display = "none";
+    y.style.display = "block";
+  }
+//  console.log(x.style.display);
+//  console.log(y.style.display);
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+
+// var y = document.getElementById("rows");
+// function changeEle(ele){
+//
+//     if (ele.id === "imgs"){
+//       document.getElementById("rows").innerHTML = "";
+//       var x = document.getElementById("check").innerHTML;
+//       document.getElementById("rows").innerHTML = x;
+//       console.log("HI");
+//     }
+//     else{
+//       console.log("HEY");
+//       document.getElementById("rows") = y;
+//     }
+// }
