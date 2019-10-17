7c11a27792326fa6cc24d51f4ea59db780259c58
Jimil Desai <jimildesai42@gmail.com>
2019-10-17 13:15:31 +0530

    New Commit (Ignore other commmits)

--- js1/myScripts.js
+++ js1/myScripts.js
@@ -9,6 +9,7 @@ function myFunc(){
   }else{
     myTopnav.classList.remove("sticky");
   }
+
 }
 function myFunction() {
   var x = document.getElementById("myTopnav");
@@ -17,6 +18,8 @@ function myFunction() {
   } else {
     x.className = "topnav";
   }
+  // console.log("HI");
+   
 }
 
 // var slideIndex = 1;
@@ -31,25 +34,26 @@ function plusSlides(n) {
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
-//
-// function showSlides(n) {
-//   var i;
-//   var slides = document.getElementsByClassName("mySlides fade");
-//   var dots = document.getElementsByClassName("dot");
-//   if (n > slides.length) {slideIndex = 1}
-//   if (n < 1) {slideIndex = slides.length}
-//   for (i = 0; i < slides.length; i++) {
-//       slides[i].style.display = "none";
-//   }
-//   for (i = 0; i < dots.length; i++) {
-//       dots[i].className = dots[i].className.replace(" active", "");
-//   }
-//   slides[slideIndex-1].style.display = "block";
-//   dots[slideIndex-1].className += " active";
-//   setTimeout(showSlides, 2000);
-// }
+
 var slideIndex = 0;
 showSlides();
+
+function showSlides1(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides fade");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
 
 function showSlides() {
   var i;
@@ -65,5 +69,5 @@ function showSlides() {
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 3000); // Change image every 2 seconds
+  setTimeout(showSlides, 3000); // Change image every 3 seconds
 }
